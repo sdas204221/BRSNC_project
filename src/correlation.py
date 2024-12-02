00c4c2f1bfde7636f23b289a6029b439ba355498
Subhra Das <sdas204221@gmail.com>
2024-12-02 21:13:22 +0530

    feat: add scatter plot visualization for correlation functions
    - Added optional scatter plot visualization to `selfCorrelationHorisontal`, `selfCorrelationVertical`, and `toImageCorrelation` functions.
    - Introduced a `scatterPlot` function using Matplotlib for pixel value correlation visualization.
    - Added `requirePlot` parameter to toggle scatter plot generation.
    - Updated `test` function for compatibility with new changes.

--- src/correlation.py
+++ src/correlation.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import statistics
 
+from matplotlib import pyplot as plt
+
 def correlation(x,y):
     n=len(x)
     xMean=statistics.mean(x)
@@ -11,14 +13,16 @@ def correlation(x,y):
     cov=cov/n
     return cov/(statistics.stdev(x)*statistics.stdev(y))
 
-def selfCorrelationHorisontal(image):
+def selfCorrelationHorisontal(image,requirePlot=True):
     grayImg = image.convert("L")
     pixel_values = list(grayImg.getdata())
     x=list(pixel_values[:len(pixel_values)-1])
     y=list(pixel_values[1:len(pixel_values)])
+    if(requirePlot):
+        scatterPlot(x,y)
     return correlation(x,y)
 
-def selfCorrelationVertical(image):
+def selfCorrelationVertical(image,requirePlot=True):
     grayImg = image.convert("L")
     img=grayImg.load()
     sizeX, sizeY=grayImg.size
@@ -28,15 +32,25 @@ def selfCorrelationVertical(image):
             pixel_values.append(img[j,i])    
     x=list(pixel_values[:len(pixel_values)-1])
     y=list(pixel_values[1:len(pixel_values)])
+    if(requirePlot):
+        scatterPlot(x,y)
     return correlation(x,y)
 
-def toImageCorrelation(image1,image2):
+def toImageCorrelation(image1,image2,requirePlot=True):
     grayImg1 = image1.convert("L")
     grayImg2 = image2.convert("L")
     x=list(grayImg1.getdata())
     y=list(grayImg2.getdata())
+    if(requirePlot):
+        scatterPlot(x,y)
     return correlation(x,y)
 
+def scatterPlot(x,y):
+    plt.scatter(x, y,marker=".", s=4)
+    plt.xlabel("X")
+    plt.ylabel("")
+    plt.title("Scatter Plot")
+    plt.show()
 
 def test():
     img1=Image.open("../data/output/scramble/0.png")
